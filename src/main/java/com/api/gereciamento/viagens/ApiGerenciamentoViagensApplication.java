7aefb649003bbe9c280fde91defcef4cfb1c2949
Marhlon Korb <marhlonkorb@hotmail.com>
2024-06-26 22:39:42 -0300

    Criação do projeto

---
+++ src/main/java/com/api/gereciamento/viagens/ApiGerenciamentoViagensApplication.java
@@ -0,0 +1,13 @@
+package com.api.gereciamento.viagens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGerenciamentoViagensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGerenciamentoViagensApplication.class, args);
+	}
+
+}
